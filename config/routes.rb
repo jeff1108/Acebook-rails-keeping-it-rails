ddf955a9f04ccbf6cd1283f96ffa8ff1e77d1cf0
Elishka Flint <elishkaf@gmail.com>
2018-08-08 16:32:47 +0100

    SET UP Add avatar routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get "session/create"
   resources :posts, :session, :users, :likes
-
+  
   root 'users#index'
 
   get    'login'   => 'session#new'
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   delete 'logout'  => 'session#destroy'
 
   resources :users do
+    resources :avatar
     resources :posts do
       resources :likes
     end
