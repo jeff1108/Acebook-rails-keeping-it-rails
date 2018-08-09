34f2566405bb8c58dcfe2ce6a9e151c31825d301
Daniel Lau <dct.lau17@gmail.com>
2018-08-09 18:26:07 +0100

    WIP: FEATURE Add submit form to create profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,25 @@
 class ProfilesController < ApplicationController
   def new
     @profile = Profile.new
+    @user = User.find(session[:current_user_id])
   end
 
   def create
+     @user = User.find(session[:current_user_id])
+     @profile = Profile.create(profile_params)
+      @profile.save
+       redirect_to user_path(@user)
+     # else
+     #   flash[:alert] = "Could not upload profile"
+     #   render :new
 
-  end
+   end
+
+   private
+
+   def profile_params
+     # params.require(:profile).permit(:summary, :image, :user_id)
+      { summary: params[:summary], image: params[:image], user_id: params[:user_id] }
+   end
 
 end
