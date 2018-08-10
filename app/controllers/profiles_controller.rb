790f37aa5104790d210a54ce6ffcd133a5d29e46
Jay Khan <jkhan992@gmail.com>
2018-08-10 03:09:24 +0100

    WIP FEATURE find Profile by user_id

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,13 +8,13 @@ class ProfilesController < ApplicationController
   def create
      @user = User.find(session[:current_user_id])
      @profile = Profile.create(profile_params)
-     if  @profile.save
+      if  @profile.save
        redirect_to user_path(@user)
        flash[:notice] = "Profile sucessfully updated"
-     else
-       flash[:alert] = "Could not upload profile"
-       render :new
-     end
+      else
+        flash[:alert] = "Could not upload profile"
+        render :new
+      end
    end
 
    def edit
@@ -25,10 +25,9 @@ class ProfilesController < ApplicationController
    def update
     @profile = Profile.where(user_id: params[:user_id])
     @user = User.find(params[:user_id])
-
     if @profile.update(profile_params)
       redirect_to user_path(@user)
-      flash[:notice] = "Profile updated222"
+      flash[:notice] = "Profile updated"
     else
       flash[:alert] = "Something wrong with your file"
       render :edit
@@ -39,7 +38,7 @@ class ProfilesController < ApplicationController
    private
 
    def profile_params
-     # params.require(:profile).permit(:avatar, :summary, :user_id)
+     # params.permit(:avatar, :summary, :user_id)
       # { image: params[:image], summary: params[:summary], user_id: params[:user_id] }
       { avatar: params[:avatar], summary: params[:summary], user_id: params[:user_id] }
    end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,5 +1,5 @@
 class Profile < ApplicationRecord
   belongs_to :users, class_name: 'User', :foreign_key => "user_id"
-  mount_uploader :avatar, ProfileUploader
 
+  mount_uploader :avatar, ProfileUploader
 end
